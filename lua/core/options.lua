b77172240caa9954fa052ebdd70a7560660b7cff
David Borzek <mail@davidborzek.de>
2023-03-31 00:19:18 +0200

    feat: add code completion

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,2 +1,4 @@
 vim.opt.termguicolors = true
 vim.opt.number = true
+vim.opt.completeopt = { "menuone", "noselect", "noinsert", "preview" }
+vim.opt.shortmess = vim.opt.shortmess + { c = true }
---
+++ lua/core/plugins/cmp.lua
@@ -0,0 +1,64 @@
+local M = {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+	},
+	config = function()
+		local cmp = require("cmp")
+
+		cmp.setup({
+			mapping = {
+				["<S-Tab>"] = cmp.mapping.select_prev_item(),
+				["<Tab>"] = cmp.mapping.select_next_item(),
+				["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.close(),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Insert,
+					select = true,
+				}),
+			},
+
+			snippet = {
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body)
+				end,
+			},
+
+			sources = {
+				{ name = "path" },
+				{ name = "nvim_lsp", keyword_length = 3 },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "nvim_lua", keyword_length = 2 },
+				{ name = "buffer", keyword_length = 2 },
+				{ name = "vsnip", keyword_length = 2 },
+			},
+
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+
+			formatting = {
+				fields = { "menu", "abbr", "kind" },
+				format = function(entry, item)
+					local menu_icon = {
+						nvim_lsp = "λ",
+						vsnip = "⋗",
+						buffer = "b",
+						path = "p",
+					}
+					item.menu = menu_icon[entry.source.name]
+					return item
+				end,
+			},
+		})
+	end,
+}
+
+return M
