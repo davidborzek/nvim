63341abaebc81945b0d792c4d3ef571bab6cc517
David Borzek <mail@davidborzek.de>
2023-03-31 14:58:11 +0200

    feat: set keymaps for buffer closing

--- lua/core/plugins/vim-bbye.lua
+++ lua/core/plugins/vim-bbye.lua
@@ -1,3 +1,23 @@
 return {
 	"vds2212/vim-bbye",
+	keys = {
+		{
+			"<C-x>",
+			"<cmd>Bdelete<cr>",
+			desc = "Close current buffer",
+			silent = true,
+		},
+		{
+			"<leader>x",
+			"<cmd>Bdelete<cr>",
+			desc = "Close current buffer",
+			silent = true,
+		},
+		{
+			"<leader>X",
+			":bufdo :Bdelete<cr>",
+			desc = "Close all buffers",
+			silent = true,
+		},
+	},
 }
