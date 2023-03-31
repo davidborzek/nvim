b55dcb1aaf86c871227bf5c013395e1979336c49
David Borzek <mail@davidborzek.de>
2023-03-31 13:58:16 +0200

    feat: set some common vim options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -2,3 +2,10 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.completeopt = { "menuone", "noselect", "noinsert", "preview" }
 vim.opt.shortmess = vim.opt.shortmess + { c = true }
+
+vim.opt.autoread = true
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
