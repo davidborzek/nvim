e8c3c2efe043193901933144a31f6801cf95ea79
David Borzek <mail@davidborzek.de>
2023-04-01 13:12:41 +0200

    feat: add keys for quit/save

--- lua/core/keys.lua
+++ lua/core/keys.lua
@@ -2,3 +2,12 @@ local map = vim.keymap.set
 
 -- clear search on escape
 map({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { silent = true })
+
+-- exit
+map("n", "<leader>q", ":qa<CR>", { desc = "Exit", silent = true })
+
+-- exit and save all
+map("n", "<leader>Q", ":wqa<CR>", { desc = "Exit and save all", silent = true })
+
+-- save all
+map("n", "<leader>w", ":wa<CR>", { desc = "Save all", silent = true })
