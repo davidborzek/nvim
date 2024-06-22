13a447e099e701e0124f6272d2cc974fed4cc65c
David Borzek <mail@davidborzek.de>
2024-06-22 19:08:25 +0200

    fix: adapt lsp cmp keyword length

--- lua/core/plugins/cmp.lua
+++ lua/core/plugins/cmp.lua
@@ -40,7 +40,7 @@ local M = {
 
 			sources = {
 				{ name = "path" },
-				{ name = "nvim_lsp", keyword_length = 3 },
+				{ name = "nvim_lsp", keyword_length = 1 },
 				{ name = "nvim_lsp_signature_help" },
 				{ name = "nvim_lua", keyword_length = 2 },
 				{ name = "buffer", keyword_length = 2 },
